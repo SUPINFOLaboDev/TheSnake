b8f283abdb780e53634a960cf17255f4650a82e1
arnaud-roche <169865@supinfo.com>
2014-11-19 19:13:16 +0100

    Création fenêtre

--- Snake.py
+++ Snake.py
@@ -1,3 +1,12 @@
-__author__ = 'Aztyu'
+import pygame
+from pygame.locals import *
 
+pygame.init()
 
+fenetre = pygame.display.set_mode((640, 480))
+
+pygame.display.flip()
+
+continuer = 1
+while continuer:
+	continuer = int(input())
