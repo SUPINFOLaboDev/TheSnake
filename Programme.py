11d133e1db010ca73f9775be097fdd6feaef151f
Raphaël Rivet <raph.rivet@gmail.com>
2014-12-03 20:21:23 +0100

    Ajout de fonction dans Progamme.py

--- Programme.py
+++ Programme.py
@@ -6,3 +6,17 @@ class MainProg:
 
     def GetInput(self):
         return input()
+
+    import pygame,sys
+    from pygame.locals import *
+
+    pygame.init()
+    fenetre = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption('Snake')
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
