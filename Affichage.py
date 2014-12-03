6d90e77f1c22e0fe567c00e16d6e095a8637e61f
cBeal <169796@supinfo.com>
2014-12-03 19:21:31 +0100

    Add function to Affichage.py

--- Affichage.py
+++ Affichage.py
@@ -1,5 +1,19 @@
 class Affichage:
 
-    def affichage_jeux(self):
+    def affichage_jeu(self):
         return 0
-        
+
+    def affichage_tableau(self):
+        return 0
+
+    def affichage_tableau(self):
+        return 0
+
+    def affichage_snake(self):
+        return 0
+
+    def affichage_pomme(self):
+        return 0
+
+    def affichage_score(self):
+        return 0
