8a36414a9ed8be6cb7d0a9ce1651ba775870672e
Marblerock <doriangailleton@msn.com>
2014-12-03 18:31:28 +0100

    Correction boucle infinie

--- Snake.py
+++ Snake.py
@@ -1,12 +1,16 @@
-import pygame
+import pygame,sys
 from pygame.locals import *
 
 pygame.init()
-
 fenetre = pygame.display.set_mode((640, 480))
-
+pygame.display.set_caption('Snake')
 pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 
-continuer = 1
-while continuer:
-	continuer = int(input())
+
+
